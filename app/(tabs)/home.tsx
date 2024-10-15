ccc40ccde329878a5c189e4296c52eecce6c0fb0
Nojus Stankevičius <Nojus869@gmail.com>
2024-10-15 22:06:55 +0300

    settings tab

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Slot, Stack } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">sit!</Text>
+      <Text className="text-lg font-black">sustok ir prisesk nx velnes</Text>
+    </View>
+  );
+}
