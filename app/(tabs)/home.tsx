7e7533877052cea78f25a240a7e028ed32c71a6f
ArnasIc3 <martinkusarnas888@gmail.com>
2024-10-16 23:44:27 +0300

    map
    small update for the view of mapper. bus

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Slot, Stack } from 'expo-router';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MapView, { Marker, UrlTile } from "react-native-maps";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">sit!</Text>
-      <Text className="text-lg font-black">sustok ir prisesk nx velnes</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 54.6872,
+          longitude: 25.2797,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <UrlTile
+          urlTemplate="https://tiles.stadiamaps.com/tiles/Stamen_toner/{z}/{x}/{y}.png"
+          maximumZ={19}
+          flipY={false}
+        />
+        <Marker
+          coordinate={{ latitude: 54.6872, longitude: 25.2797 }}
+          title={"Marker Title"}
+          description={"Marker Description"}
+        />
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
