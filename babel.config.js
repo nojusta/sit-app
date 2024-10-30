84bd4a3eb3ebb42f6f56dc042ee78007a7aaf4ed
ArnasIc3 <martinkusarnas888@gmail.com>
2024-10-30 22:44:32 +0200

    safety issues
    backend dum dum

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
