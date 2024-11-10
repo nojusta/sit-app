10a08726333b0cf34843567c6e17aeac1bd0e59f
ArnasIc3 <martinkusarnas888@gmail.com>
2024-11-11 01:20:21 +0200

    marker addition (better in future)

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,26 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, StyleSheet, Image } from "react-native";
-import MapView, { UrlTile, Region, Marker } from "react-native-maps";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Alert,
+  Text,
+  Platform,
+  TextInput,
+} from "react-native";
+import MapView, {
+  UrlTile,
+  Region,
+  Marker,
+  PROVIDER_GOOGLE,
+  PROVIDER_DEFAULT,
+} from "react-native-maps";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ClusteredMapView from "react-native-map-clustering";
 import InfoWindow from "../../components/InfoWindow"; // Import the InfoWindow component
 import { useMarkerContext } from "../../context/MarkerContext";
+import * as Location from "expo-location";
 
 const INITIAL_INFO_WINDOW_HEIGHT = 100; // Initial height of the info window
 
@@ -13,6 +29,13 @@ const HomeApp: React.FC = () => {
   const [zoomLevel, setZoomLevel] = useState<number>(10); // State to track the zoom level
   const [selectedMarker, setSelectedMarker] = useState<any>(null); // State to track the selected marker
   const [lastRegion, setLastRegion] = useState<Region | null>(null); // State to store the last region before zooming into a marker
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  ); // State to store the user's location
+  const [errorMsg, setErrorMsg] = useState<string | null>(null); // State to store error messages
+  const [userMarker, setUserMarker] = useState<Region | null>(null); // State to store the user's marker
+  const [markerInfo, setMarkerInfo] = useState<string>(""); // State to store the marker information
+  const [showInputBox, setShowInputBox] = useState<boolean>(false); // State to show/hide the input box
   const mapRef = useRef<MapView>(null); // Reference to the MapView
 
   // Define the initial region for the map
@@ -22,6 +45,20 @@ const HomeApp: React.FC = () => {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
+
+  // Request location permissions and get the user's current location
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
   // Handle region change complete event
   const handleRegionChangeComplete = (region: Region): void => {
@@ -65,13 +102,74 @@ const HomeApp: React.FC = () => {
 
   // Handle map press event
   const handleMapPress = () => {
-    if (selectedMarker) {
+    if (selectedMarker || userMarker || showInputBox) {
       setSelectedMarker(null);
+      setUserMarker(null);
+      setShowInputBox(false);
       setIsMarkerSelected(false);
       if (lastRegion) {
         mapRef.current?.animateToRegion(lastRegion, 800);
       }
     }
+  };
+
+  // Handle button press to center the map on the user's location
+  const handleCenterOnUserLocation = () => {
+    if (location && mapRef.current) {
+      mapRef.current.animateToRegion(
+        {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        },
+        1000
+      );
+    } else {
+      Alert.alert(
+        "Location not available",
+        "Unable to get your current location."
+      );
+    }
+  };
+
+  // Handle button press to add a marker at the user's location
+  const handleAddMarker = () => {
+    if (location) {
+      setUserMarker({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      });
+    } else {
+      Alert.alert(
+        "Location not available",
+        "Unable to get your current location."
+      );
+    }
+  };
+
+  // Handle long press on the map to add a marker
+  const handleLongPress = (e: any) => {
+    const coordinate = e.nativeEvent.coordinate;
+    setUserMarker({
+      latitude: coordinate.latitude,
+      longitude: coordinate.longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    });
+    setShowInputBox(true); // Show the input box
+  };
+
+  // Handle marker drag end event
+  const handleMarkerDragEnd = (e: any) => {
+    setUserMarker({
+      latitude: e.nativeEvent.coordinate.latitude,
+      longitude: e.nativeEvent.coordinate.longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    });
   };
 
   // Multiple Marker data
@@ -113,7 +211,7 @@ const HomeApp: React.FC = () => {
     container: {
       width: 40,
       height: 40,
-      borderRadius: 20,
+      borderRadius: 30,
       backgroundColor: "black",
       justifyContent: "center",
       alignItems: "center",
@@ -130,10 +228,16 @@ const HomeApp: React.FC = () => {
       <View style={styles.container}>
         <ClusteredMapView
           ref={mapRef}
+          provider={
+            Platform.OS === "android" ? PROVIDER_GOOGLE : PROVIDER_DEFAULT
+          }
           style={styles.map}
           initialRegion={initialRegion}
           onRegionChangeComplete={handleRegionChangeComplete}
           onPress={handleMapPress}
+          onLongPress={handleLongPress} // Handle long press on the map
+          showsUserLocation={true}
+          showsMyLocationButton={false}
           clusterColor="black"
           clusterTextColor="white"
           minimumClusterSize={5}
@@ -152,10 +256,18 @@ const HomeApp: React.FC = () => {
             >
               <Image
                 source={require("../../assets/images/custom-marker.png")}
-                style={{ width: 30, height: 30 }}
+                style={{ width: 40, height: 40 }}
               />
             </Marker>
           ))}
+          {userMarker && (
+            <Marker
+              coordinate={userMarker}
+              draggable
+              onDragEnd={handleMarkerDragEnd}
+              title={"Your Marker"}
+            />
+          )}
         </ClusteredMapView>
         {selectedMarker && (
           <InfoWindow
@@ -166,6 +278,34 @@ const HomeApp: React.FC = () => {
             initialHeight={INITIAL_INFO_WINDOW_HEIGHT}
           />
         )}
+        {showInputBox && (
+          <View style={styles.inputBox}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter location information"
+              value={markerInfo}
+              onChangeText={setMarkerInfo}
+            />
+            <TouchableOpacity
+              style={styles.saveButton}
+              onPress={() => {
+                Alert.alert("Marker Info", markerInfo);
+                setShowInputBox(false); // Hide the input box
+              }}
+            >
+              <Text style={styles.saveButtonText}>Save</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        <TouchableOpacity
+          style={styles.centerButton}
+          onPress={handleCenterOnUserLocation}
+        >
+          <Text style={styles.centerButtonText}>⌖</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.addButton} onPress={handleAddMarker}>
+          <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaProvider>
   );
@@ -177,6 +317,67 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
+  },
+  centerButton: {
+    position: "absolute",
+    bottom: 90,
+    right: 20,
+    backgroundColor: "gray",
+    padding: 10,
+    borderRadius: 50,
+  },
+  centerButtonText: {
+    color: "white",
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  inputBox: {
+    position: "absolute",
+    bottom: 180,
+    left: 20,
+    right: 20,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  saveButton: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  saveButtonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  addButton: {
+    position: "absolute",
+    bottom: 90,
+    left: 20,
+    backgroundColor: "rgba(0, 128, 0, 0.5)", // Green color with 50% opacity
+    paddingVertical: 15, // Adjust vertical padding
+    paddingHorizontal: 20, // Adjust horizontal padding
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 40, // Adjust font size if needed
+    lineHeight: 40, // Adjust line height to match font size
   },
 });
 
